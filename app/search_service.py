97428f527db6c90294bfb0e35ada90e86ee9fff9
kristyyip <59596642+kristyyip@users.noreply.github.com>
2020-04-29 14:53:30 -0400

    Accept user input, fix rating error

--- app/search_service.py
+++ app/search_service.py
@@ -12,9 +12,18 @@ load_dotenv()
 # INFO INPUTS
 #
 
+movie = input("Please enter a movie title (i.e. Iron Man): ") # accept user input
+
+# how to replace whitespaces (source): https://stackoverflow.com/questions/1007481/how-do-i-replace-whitespaces-with-underscore-and-vice-versa
+movie.replace(" ","+") # transforms the user input so that it is suitable for the request url later
+
 OMDB_API_KEY = os.environ.get("OMDB_API_KEY")
-request_url = f"https://omdbapi.com/?t=Iron+Man&apikey={OMDB_API_KEY}"
+request_url = f"https://omdbapi.com/?t={movie}&apikey={OMDB_API_KEY}"
 response = requests.get(request_url)
+
+if "\"Response\":\"False\"" in response.text:
+    print("Oops, couldn't find that movie. Please try again.")
+    exit()
 parsed_response = json.loads(response.text)
 
 title = parsed_response["Title"]
@@ -23,11 +32,23 @@ genre = parsed_response["Genre"]
 director = parsed_response["Director"]
 cast = parsed_response["Actors"]
 summary = parsed_response["Plot"]
-rating = parsed_response["Ratings"][1]["Value"]
+
+if "Rotten Tomatoes" in parsed_response["Ratings"]:
+    rating = parsed_response["Ratings"][1]["Value"]
+else:
+    rating = parsed_response["Ratings"][0]["Value"]
+
+#
+# INFO OUTPUTS
+#
 
 print(f"TITLE: {title} ({release_year})")
 print(f"GENRE: {genre}")
 print(f"DIRECTOR: {director}")
 print(f"MAIN CAST: {cast}")
 print(f"SUMMARY: {summary}")
-print(f"ROTTEN TOMATOES RATING: {rating}")
+
+if "Rotten Tomatoes" in parsed_response["Ratings"]:
+    print(f"ROTTEN TOMATOES RATING: {rating}")
+else:
+    print(f"IMDb RATING: {rating}")
