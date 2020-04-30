b38f6bd58b16b1c952deb533f91d74112a967ad3
kristyyip <59596642+kristyyip@users.noreply.github.com>
2020-04-30 02:55:28 -0400

    Change request url so it allows user to find the correct title amongst similar titles

--- app/search_service.py
+++ app/search_service.py
@@ -8,47 +8,81 @@ import requests
 
 load_dotenv()
 
-#
-# INFO INPUTS
-#
+if __name__ == "__main__":
 
-movie = input("Please enter a movie title (i.e. Iron Man): ") # accept user input
+    #
+    # INFO INPUTS
+    #
 
-# how to replace whitespaces (source): https://stackoverflow.com/questions/1007481/how-do-i-replace-whitespaces-with-underscore-and-vice-versa
-movie.replace(" ","+") # transforms the user input so that it is suitable for the request url later
+    title = input("Please enter a movie or tv show title (i.e. 'Iron Man' or 'Game of Thrones'): ") # accept user input
 
-OMDB_API_KEY = os.environ.get("OMDB_API_KEY")
-request_url = f"https://omdbapi.com/?t={movie}&apikey={OMDB_API_KEY}"
-response = requests.get(request_url)
+    # how to replace whitespaces (source): https://stackoverflow.com/questions/1007481/how-do-i-replace-whitespaces-with-underscore-and-vice-versa
+    title.replace(" ","+") # transforms the user input so that it is suitable for the request url later
 
-if "\"Response\":\"False\"" in response.text:
-    print("Oops, couldn't find that movie. Please try again.")
-    exit()
-parsed_response = json.loads(response.text)
+    OMDB_API_KEY = os.environ.get("OMDB_API_KEY")
+    request_url = f"https://omdbapi.com/?s={title}&apikey={OMDB_API_KEY}"
+    response = requests.get(request_url)
 
-title = parsed_response["Title"]
-release_year = parsed_response["Year"]
-genre = parsed_response["Genre"]
-director = parsed_response["Director"]
-cast = parsed_response["Actors"]
-summary = parsed_response["Plot"]
+    # validating user's input
+    if "\"Response\":\"False\"" in response.text:
+        print("Oops, couldn't find that movie. Please try again.")
+        exit()
 
-if "Rotten Tomatoes" in parsed_response["Ratings"]:
-    rating = parsed_response["Ratings"][1]["Value"]
-else:
-    rating = parsed_response["Ratings"][0]["Value"]
+    parsed_response = json.loads(response.text)
 
-#
-# INFO OUTPUTS
-#
+    sr = parsed_response["Search"] # search results
 
-print(f"TITLE: {title} ({release_year})")
-print(f"GENRE: {genre}")
-print(f"DIRECTOR: {director}")
-print(f"MAIN CAST: {cast}")
-print(f"SUMMARY: {summary}")
 
-if "Rotten Tomatoes" in parsed_response["Ratings"]:
-    print(f"ROTTEN TOMATOES RATING: {rating}")
-else:
-    print(f"IMDb RATING: {rating}")
+    # if there is more than one result, then the program will have the user choose the title they were looking for
+    # from a list; otherwise, the program will go ahead and display the information of the one found result
+    if int(parsed_response["totalResults"]) > 1:
+        
+        org_list = []
+
+        for t in sr:
+            org_list.append(t["Title"] + ", " + t["Year"])
+            print(t["Title"] + " (" + t["Year"] +")") # prints a list of the search results' title and release year
+
+        print("----------------------------------")
+        correct_search = input("From the list above, what title were you looking for? Please write in the format of Title, Year (i.e. 'The Avengers, 1998'): ")
+        
+        if (correct_search) in org_list:
+            i = org_list.index(correct_search) # finds the index of the title the user is looking for in org_list so it can be matched with the one in the parsed_response
+            id = sr[i]["imdbID"] 
+        else:
+            print("That's not an option from the list above. Please try again.")
+    else:
+        id = sr[0]["imdbID"]
+
+
+    # make another request to match ids
+    request_url = f"https://omdbapi.com/?i={id}&apikey={OMDB_API_KEY}"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+
+    title_name = parsed_response["Title"]
+    release_year = parsed_response["Year"]
+    genre = parsed_response["Genre"]
+    director = parsed_response["Director"]
+    cast = parsed_response["Actors"]
+    summary = parsed_response["Plot"]
+
+    # rating preference on Rotten Tomatoes, but will resort to IMDb's rating if non-existent
+    if "Rotten Tomatoes" in parsed_response["Ratings"][1]["Source"]:
+        rating = "ROTTEN TOMATOES RATING: " + parsed_response["Ratings"][1]["Value"]
+    else:
+        rating = "IMDb RATING: " + parsed_response["Ratings"][0]["Value"]
+
+    #
+    # INFO OUTPUTS
+    #
+
+    print("----------------------------------")
+    print(f"TITLE: {title_name} ({release_year})")
+    print("----------------------------------")
+
+    print(f"GENRE: {genre}")
+    print(f"DIRECTOR: {director}")
+    print(f"MAIN CAST: {cast}")
+    print(f"SUMMARY: {summary}")
+    print(rating)
