6997444c128a5771ddf9eb633e25960ef57393c0
kristyyip <59596642+kristyyip@users.noreply.github.com>
2020-05-03 02:52:13 -0400

    Testing get response

--- test/search_test.py
+++ test/search_test.py
@@ -1,4 +1,21 @@
-from app.search_service import youtube_search
+import pytest
+import os
+
+from app.search_service import get_response, youtube_search
+
+CI_ENV = os.environ.get("CI") == "true" # expect default environment variable setting of "CI=true" on Travis CI, see: https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+
+@pytest.mark.skipif(CI_ENV==True, reason="to avoid configuring credentials on, and issuing requests from, the CI server")
+def test_get_response():
+    title = "Iron Man"
+
+    parsed_response = get_response(title)
+
+    assert isinstance(parsed_response, dict)
+    assert "Search" in parsed_response.keys()
+    assert "totalResults" in parsed_response.keys()
+    assert "Response" in parsed_response.keys()
+    assert parsed_response["Response"] == "True"
 
 def test_youtube_search():
     options = "Iron Man"
