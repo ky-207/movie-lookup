bae35c9699a0fdd47a78933ffe8d2f81042811b6
kristyyip <59596642+kristyyip@users.noreply.github.com>
2020-05-02 22:39:23 -0400

    YouTube Search functionality: accepting user choice and moving function to top

--- app/search_service.py
+++ app/search_service.py
@@ -6,7 +6,51 @@ import json
 from dotenv import load_dotenv
 import requests
 
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import webbrowser
+
 load_dotenv()
+
+YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY")
+YOUTUBE_API_SERVICE_NAME='youtube'
+YOUTUBE_API_VERSION='v3'
+
+# brings up YouTube trailer
+def youtube_search(options):
+    """
+    Searches and opens a YouTube video in your browser
+    
+    Source: https://github.com/youtube/api-samples/blob/master/python/search.py
+
+    Param: options (str) like "Iron Man"
+
+    Example: youtube_search("Iron Man")
+
+    Returns: None
+    """
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+        developerKey=YOUTUBE_API_KEY)
+
+    # Call the search.list method to retrieve results matching the specified query term.
+    search_response = youtube.search().list(
+        q=options + " trailer",
+        part='id,snippet',
+        maxResults=1
+    ).execute()
+
+    # Add each result to the appropriate list, and then display the lists of matching videos, channels, and playlists.
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':                 
+            #print ('YouTube Trailer:\n', search_result['snippet']['title'])
+            #print(' ' + 'https://www.youtube.com/watch?v=' + search_result['id']['videoId'])
+            print("----------------------------------")
+            webbrowser.open('https://www.youtube.com/watch?v=' + search_result['id']['videoId'])
+        else:
+            print('Sorry, a trailer could not be found for this movie.')
+            print("----------------------------------")
+
+    return None
 
 if __name__ == "__main__":
 
@@ -91,14 +135,18 @@ if __name__ == "__main__":
 
 
 
-    #Youtube search
-    from googleapiclient.discovery import build
-    from googleapiclient.errors import HttpError
+    #Youtube search    
+    while True:
+        youtube = input(f"Would you like to see a trailer for {title_name}? Enter 'Yes' or 'No': ")
+        if youtube.lower() == "yes":
+            print("Okay, pulling up trailer now...")
+            youtube_search(correct_search)
+            break
+        elif youtube.lower() == "no":
+            print("Okay, you opted not to watch the trailer.")
+            print("----------------------------------")
 
-    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY")
-    YOUTUBE_API_SERVICE_NAME='youtube'
-    YOUTUBE_API_VERSION='v3'
-
+<<<<<<< Updated upstream
     #Source: https://github.com/youtube/api-samples/blob/master/python/search.py
     def youtube_search(options):
         youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
@@ -120,4 +168,9 @@ if __name__ == "__main__":
                 print('Sorry, a trailer could not be found for this movie.')
 
     youtube_search(correct_search)
+=======
+            break
+        else:
+            print("Sorry, that is an invalid response. Please try again and enter 'Yes' or 'No'.")
+>>>>>>> Stashed changes
 
